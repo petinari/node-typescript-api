737f2e65ff1de4c78a19467bbf122d332984f514
Robson <petinari@outlook.com>
2023-03-17 11:38:19 +0000

    17032023

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -1,5 +1,5 @@
 describe('Beach forecast functional tests', () => {
-  it('Should return a forecast just a few times', async () => {
+  it('Should return a forecast just a few times.', async () => {
     const { body, status } = await global.testRequest.get('/forecast');
     expect(status).toBe(200);
     expect(body).toEqual({});
